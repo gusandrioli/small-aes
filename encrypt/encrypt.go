0a000089307ca554c061e65a55a5af1bd8e047d1
gusandrioli <gusaandrioli@gmail.com>
2020-11-23 23:56:04 -0800

    rm comments

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -34,14 +34,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// here we encrypt our text using the Seal function
-	// Seal encrypts and authenticates plaintext, authenticates the
-	// additional data and appends the result to dst, returning the updated
-	// slice. The nonce must be NonceSize() bytes long and unique for all
-	// time, for a given key.
-
-	//fmt.Println(gcm.Seal(nonce, nonce, text, nil))
-
 	err = ioutil.WriteFile("myfile.data", gcm.Seal(nonce, nonce, text, nil), 0777)
 	if err != nil {
 		log.Fatal(err)
